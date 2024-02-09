1da794c05559b20b5db32704f435dae06a5817f6
Muhammad Abdullah Abid <abd@abd.dev>
2024-02-08 21:49:57 -0800

    first commit

---
+++ rahnuma/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from rahnuma.users.models import User
+from rahnuma.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
